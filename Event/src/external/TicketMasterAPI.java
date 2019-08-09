7e429e90eeec3c4b8a0bf45f8fe053d737cb60d0
Marilyn <yml.100205@gmail.com>
2019-08-09 10:56:54 -0400

    Purify data and convert data from JASONArray to a list of items object

--- Event/src/external/TicketMasterAPI.java
+++ Event/src/external/TicketMasterAPI.java
@@ -5,10 +5,19 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.net.URL;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
+
 
 public class TicketMasterAPI {
 	private static final String URL = "https://app.ticketmaster.com/discovery/v2/events.json";
@@ -84,6 +93,125 @@ public class TicketMasterAPI {
 		}
 		return new JSONArray();
 	}
+	
+	//use Item class in TicketMasterAPI to get clean data
+	//convert events in JSONArray type to List<Item> objects
+	private List<Item> getItemList(JSONArray events) throws JSONException {
+		List<Item> itemList = new ArrayList<>();
+		
+		for (int i = 0; i < events.length(); i++) {
+			JSONObject event = events.getJSONObject(i);
+			ItemBuilder builder = new ItemBuilder();
+			if (!event.isNull("id")) {
+				builder.setItemId(event.getString("id"));
+			}
+			if (!event.isNull("name")) {
+				builder.setName(event.getString("name"));
+			}
+			if (!event.isNull("url")) {
+				builder.setUrl(event.getString("url"));
+			}
+			if (!event.isNull("distance")) {
+				builder.setDistance(event.getDouble("distance"));
+			}
+			//TicketMaster does not provide rating now
+			//just leave it here for now, it will return 0 by default
+			if (!event.isNull("rating")) {
+				builder.setRating(event.getDouble("rating"));
+			}
+			builder.setAddress(getAddress(event));
+			builder.setImageUrl(getImageUrl(event));
+			builder.setCategories(getCategories(event));
+			//put the item
+			itemList.add(builder.build());
+		}
+		return itemList;
+	}
+	
+	/*
+	 * Some helper functions to get data that is deep (not directly under events)
+	 * 1. fetch address
+	 * 2. fetch imageUrl
+	 * 3. fetch categories
+	 */
+	
+	//fetch address
+	private String getAddress(JSONObject event) throws JSONException {
+		if (!event.isNull("_embedded")) {
+			JSONObject embedded = event.getJSONObject("_embedded");
+			if (!embedded.isNull("venues")) {
+				JSONArray venues = embedded.getJSONArray("venues");
+				for (int i = 0; i < venues.length(); i++) {
+					JSONObject venue = venues.getJSONObject(i);
+					StringBuilder addressBuilder = new StringBuilder();
+					//get address
+					if (!venue.isNull("address")) {
+						JSONObject address = venue.getJSONObject("address");
+						if (!address.isNull("line1")) {
+							addressBuilder.append(address.get("line1"));
+						}
+						if (!address.isNull("line2")) {
+							addressBuilder.append(",");
+							addressBuilder.append(address.get("line2"));
+						}
+						if (!address.isNull("line3")) {
+							addressBuilder.append(",");
+							addressBuilder.append(address.get("line3"));
+						}
+					}
+					//get city
+					if (!venue.isNull("city")) {
+						JSONObject city = venue.getJSONObject("city");
+						if (!city.isNull("name")) {
+							addressBuilder.append(",");
+							addressBuilder.append(city.getString("name"));
+						}
+					}
+					
+					//get full address
+					String addressStr = addressBuilder.toString();
+					if (!addressStr.equals("")) {
+						return addressStr;
+					}
+				}
+			}
+		}
+		return "";	
+	}
+	
+	//fetch imageUrl
+	private String getImageUrl(JSONObject event) throws JSONException{
+		if (!event.isNull("images")) {
+			JSONArray images = event.getJSONArray("images");
+			for (int i = 0; i < images.length(); i++) {
+				JSONObject image = images.getJSONObject(i);
+				if (!image.isNull("url")) {
+					return image.getString("url");
+				}	
+			}
+		}
+		return "";
+	}
+	
+	//fetch categories
+	private Set<String> getCategories(JSONObject event) throws JSONException {
+		Set<String> categories = new HashSet<>();
+		if (!event.isNull("Classifications")) {
+			JSONArray classifications = event.getJSONArray("Classifications");
+			for (int i = 0; i < classifications.length(); i++) {
+				JSONObject classification = classifications.getJSONObject(i);
+				if (!classification.isNull("segment")) {
+					JSONObject segment = classification.getJSONObject("segment");
+					if (!segment.isNull("name")) {
+						categories.add(segment.getString("name"));
+					}
+				}
+			}
+		}
+		return categories;
+	}
+	
+	
 	
 	private void queryAPI(double lat, double lon) {
 		JSONArray events = search(lat, lon, null);
