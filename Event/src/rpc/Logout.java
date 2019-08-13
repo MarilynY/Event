10c87680204ac84f3ed756625d4a10d2a09667d9
Marilyn <yml.100205@gmail.com>
2019-08-13 14:14:55 -0400

    delete unused import

--- Event/src/rpc/Logout.java
+++ Event/src/rpc/Logout.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import db.DBConnection;
-import db.DBConnectionFactory;
 
 /**
  * Servlet implementation class Logout
