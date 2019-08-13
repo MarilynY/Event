2f9cfe60235df3a2bcd2bc9b3890b7eaf3165c45
Marilyn <yml.100205@gmail.com>
2019-08-13 18:07:48 -0400

    metadata

--- Event/src/rpc/RecommendItem.java
+++ Event/src/rpc/RecommendItem.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RecommendItem
  */
-@WebServlet("/Recommend")
+@WebServlet("/recommend")
 public class RecommendItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
